3619adcfb44c389f6857610816df8f709a89281a
Kristina <ms.krasnodubskaya@mail.ru>
2020-04-18 17:14:25 +0900

    use bitmaps

--- app/src/main/java/com/example/memorycanvas/TilesView.java
+++ app/src/main/java/com/example/memorycanvas/TilesView.java
@@ -1,9 +1,10 @@
 package com.example.memorycanvas;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
 import android.os.AsyncTask;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -18,26 +19,25 @@ import java.util.Arrays;
 import java.util.Random;
 
 class Card {
-    Paint p = new Paint();
-    int color;
-    int backColor = Color.GRAY;
+    Bitmap picture, backColor;
+    int path2pic;
     boolean isOpen = false; // цвет карты
     float x, y, width, height;
 
-    public Card(float x, float y, float width, float height, int color) {
-        this.color = color;
+    public Card(float x, float y, float width, float height, Bitmap picture, int path, Bitmap backColor) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
+        this.picture = picture;
+        this.path2pic = path;
+        this.backColor = backColor;
     }
 
     public void draw(Canvas c) {
-        // нарисовать карту в виде цветного прямоугольника
         if (isOpen) {
-            p.setColor(color);
-        } else p.setColor(backColor);
-        c.drawRect(x, y, x + width, y + height, p);
+            c.drawBitmap(picture, x, y, null);
+        } else c.drawBitmap(backColor, x, y, null);
     }
 
     public boolean flip(float touch_x, float touch_y) {
@@ -61,8 +61,8 @@ public class TilesView extends View {
 
     boolean init_field = false;
     int k1 = 4, k2 = 3; // 4x3
-    int[][] tiles;  // массив 2*n цветов
-    Card firstCard, secondCard;
+    int[][] tiles;  // массив 2*n адресов картинок
+    int firstCard, secondCard;
     boolean stop_draw = false;
 
     public TilesView(Context context) {
@@ -71,11 +71,11 @@ public class TilesView extends View {
 
     public TilesView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        // заполнить массив tiles случайными цветами
         // сгенерировать поле 2*n карт, при этом
-        // должно быть ровно n пар карт разных цветов
+        // должно быть ровно n пар карт разных картинок
 
-        int[] rgb = new int[]{Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW, Color.CYAN, Color.MAGENTA};
+        int[] rgb = new int[]{R.drawable.card1, R.drawable.card2, R.drawable.card3,
+                R.drawable.card4, R.drawable.card5, R.drawable.card6};
         int[] pair = new int[rgb.length];
         Arrays.fill(pair, 2);
 
@@ -102,32 +102,48 @@ public class TilesView extends View {
         int f1 = (int) ((Math.min(w, h) - a * k2 - offset * (k2 - 1)) / 2); //offset_for_center
         int f2 = (int) ((Math.max(w, h) - b * k1 - offset * (k1 - 1)) / 2); //offset_for_center
 
+        Bitmap img = BitmapFactory.decodeResource(getResources(), R.drawable.card_back);
+        Bitmap back = Bitmap.createScaledBitmap(img,a,b,false);
         for (int i = 0; i < k1; i++) {
             for (int j = 0; j < k2; j++) {
-                int p = tiles[i][j];
-                cards.add(new Card((a + offset) * j + f1, (b + offset) * i + f2, a, b, p));
+                Bitmap image = BitmapFactory.decodeResource(getResources(), tiles[i][j]);
+                Bitmap pic = Bitmap.createScaledBitmap(image, a, b, false);
+                cards.add(new Card((a + offset) * j + f1, (b + offset) * i + f2, a, b, pic, tiles[i][j], back));
             }
         }
     }
 
-    public void checkOpenCardsEqual(Card card1, Card card2) {
-        if (card1.color == card2.color) {
-            cards.remove(card1);
-            cards.remove(card2);
+    public void checkOpenCardsEqual(int p1, int p2) {
+        Card c1 = null, c2 = null;
+        if (p1 == p2) {
+            int k = 2;
+            for (Card c : cards) {
+                if (c.path2pic == p1 && k == 2) {
+                    c1 = c;
+                    k--;
+                }else if (c.path2pic == p1 && k == 1) {
+                    c2 = c;
+                    k--;
+                    break;
+                }
+            }
+            cards.remove(c1);
+            cards.remove(c2);
         }
     }
 
-    public void setWinBackground(){
+    public void setWinBackground() {
         setBackground(getResources().getDrawable(R.drawable.bg_congratulation));
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        // отрисовка плиток
+
         width = canvas.getWidth();
         height = canvas.getHeight();
 
+        // отрисовка плиток
         if (!init_field) {
             init(width, height);
             init_field = true;
@@ -140,7 +156,7 @@ public class TilesView extends View {
                 c.draw(canvas);
             }
         } else {
-            if(!stop_draw){
+            if (!stop_draw) {
                 stop_draw = true;
                 setWinBackground();
                 Toast.makeText(getContext(), "Поздравляем!!", Toast.LENGTH_LONG).show();
@@ -162,7 +178,7 @@ public class TilesView extends View {
                         Log.d("mytag", "card flipped: " + openedCard);
                         openedCard++;
                         invalidate();
-                        firstCard = c;
+                        firstCard = c.path2pic;
                         return true;
                     }
                 }
@@ -171,7 +187,7 @@ public class TilesView extends View {
                     // перевернуть карту с задержкой
                     if (c.flip(x, y)) {
                         openedCard++;
-                        secondCard = c;
+                        secondCard = c.path2pic;
                         invalidate();
                         PauseTask task = new PauseTask();
                         task.execute(PAUSE_LENGTH);
@@ -186,7 +202,7 @@ public class TilesView extends View {
 
     public void newGame() {
         // запуск новой игры
-        TilesView tl = new TilesView(getContext(),null);
+        TilesView tl = new TilesView(getContext(), null);
         init_field = false;
         this.tiles = tl.tiles;
         this.cards = tl.cards;
@@ -217,7 +233,7 @@ public class TilesView extends View {
             }
             openedCard = 0;
             isOnPauseNow = false;
-            // если открылись карты одинакового цвета, удалить их из списка
+            // если открылись одинаковые карты, удалить их из списка
             checkOpenCardsEqual(firstCard, secondCard);
             invalidate();
         }
